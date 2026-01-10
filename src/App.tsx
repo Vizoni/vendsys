8ad97efa766c717af814774934f38a582b580a5d
Raphael Vizoni do Prado <ravizoni@gmail.com>
2026-01-10 08:18:27 -0300

    feat: wip select region

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Button } from './components/ui/button';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './components/ui/select';
+import {
+  Field,
+  FieldLabel,
+  FieldDescription,
+  FieldGroup,
+  FieldLegend,
+  FieldSet,
+} from './components/ui/field';
+import { Checkbox } from './components/ui/checkbox';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className='flex gap-4 p-4'>
+        <Button variant='ghost'>New</Button>
+        <Button variant='ghost'>Delete</Button>
+      </section>
+      <section className='p-4 gap-4 flex flex-row'>
+        <Field className='flex-row items-center'>
+          <FieldLabel htmlFor='region'>Region</FieldLabel>
+          <Select defaultValue=''>
+            <SelectTrigger id='region'>
+              <SelectValue placeholder='Choose region' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='north'>North</SelectItem>
+              <SelectItem value='east'>East</SelectItem>
+              <SelectItem value='west'>West</SelectItem>
+              <SelectItem value='south'>South</SelectItem>
+            </SelectContent>
+          </Select>
+        </Field>
+        <FieldSet className='flex flex-row'>
+          <FieldLegend>Include Inactive</FieldLegend>
+          <FieldGroup>
+            <Field orientation='horizontal'>
+              <Checkbox id='include-inactive' defaultChecked />
+            </Field>
+          </FieldGroup>
+        </FieldSet>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
